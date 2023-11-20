ada0181f0aa675d51d79c4de55afc88bb29227fc
Admin <praveen.trainer@outlook.com>
2023-11-20 11:12:23 +0530

    using the image compoenent

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ListingDynamicComponent />
+      <Image 
+      width={640}
+      height={360}
+      placeholder='blur'
+      blurDataURL='https://dummyimage.com/64x36/000/aaa'
+      src='https://dummyimage.com/6400x3600/fff/aaa' alt="Placeholder image"/>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    images : {
+        domains: [
+            "dummyimage.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
