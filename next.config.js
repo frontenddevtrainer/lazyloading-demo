30a5f5c6831513f73c7cbc1c7bb06d224ef62823
Admin <praveen.trainer@outlook.com>
2023-11-20 11:22:36 +0530

    next confif update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "dummyimage.com",
+          pathname: "**",
+        },
+      ],
+    },
+  };
 
-    images : {
-        domains: [
-            "dummyimage.com"
-        ]
-    }
-}
-
-module.exports = nextConfig
+  module.exports = nextConfig
