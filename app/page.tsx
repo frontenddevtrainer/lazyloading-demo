559f809acaa3b289891ae5b3b67b8ec4384c3968
Admin <praveen.trainer@outlook.com>
2023-11-20 10:48:49 +0530

    lazy loading on client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import dynamic from 'next/dynamic';
 
-const ListingDynamicComponent = dynamic(() => import("../components/listing"))
+const ListingDynamicComponent = dynamic(() => import("../components/listing"),
+  { loading: () => <p>Loading</p>, ssr: false }
+)
 
 export default function Home() {
   return (
--- components/listing.tsx
+++ components/listing.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function Listing(){
     return <p>Listing component</p>
 }
